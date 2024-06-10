fd6a2b7242581c24de9f6afff8491b214656d95c
limaotong <maotong06@qq.com>
2024-06-10 16:53:14 +0800

    feat: 修改version 图标，名称

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,39 +1,7 @@
 ## Change Log
 
-### 1.2.0
+### 2.0.0
 
-#### Features
-
-- Ability to add an editor tab to an already created group by right clicking on the editor tab and selecting "Add to a Minimized Group" (requested by [stagefright5](https://github.com/suhay/vscode-editor-group-minimizer/issues/10))
-- Ability to remove single, minimized documents from within the group tree by right clicking on the document and selecting "Remove from Group" (requested by [stagefright5](https://github.com/suhay/vscode-editor-group-minimizer/issues/10))
-
-#### Bugs
-
-- [Issue #13](https://github.com/suhay/vscode-editor-group-minimizer/issues/13): NaN in the new group title
-- [Issue #12](https://github.com/suhay/vscode-editor-group-minimizer/issues/12): Conflict with pinned tabs
-
-- Security Updates
-
-### 1.1.0
-
-#### Features
-
-- Rename minimized groups (requested by [vince-roll20](https://github.com/suhay/vscode-editor-group-minimizer/issues/5))
-
-### 1.0.3
-
-#### Bugs
-
-- [Issue #3](https://github.com/suhay/vscode-editor-group-minimizer/issues/3): Not saving all tabs
-
-### 1.0.2
-
-- Bug fixes
-
-### 1.0.1
-
-- Fixing package
-
-### 1.0.0
-
-- Initial release
+- 增加了添加到已有group
+- 增加了文件图标显示
+- 增加对文件点击打开
--- src/extension.ts
+++ src/extension.ts
@@ -5,13 +5,13 @@ import { EditorGroupTreeDataProvider } from './editorGroupTreeDataProvider';
 export function activate(context: vscode.ExtensionContext) {
   const editorGroupTreeDataProvider = new EditorGroupTreeDataProvider(context);
   vscode.window.registerTreeDataProvider('minimizedGroups', editorGroupTreeDataProvider);
-  vscode.commands.registerCommand('vscode-editor-group-minimizer.minimize', () => editorGroupTreeDataProvider.minimize());
-  vscode.commands.registerCommand('vscode-editor-group-minimizer.minimizeAsAdd', () => editorGroupTreeDataProvider.minimizeAsAdd());
-  vscode.commands.registerCommand('vscode-editor-group-minimizer.remove', group => editorGroupTreeDataProvider.remove(group));
-  vscode.commands.registerCommand('vscode-editor-group-minimizer.restore', group => editorGroupTreeDataProvider.restore(group));
-  vscode.commands.registerCommand('vscode-editor-group-minimizer.rename', group => editorGroupTreeDataProvider.rename(group));
-  vscode.commands.registerCommand('vscode-editor-group-minimizer.addToGroup', uri => editorGroupTreeDataProvider.addToGroup(uri));
-  vscode.commands.registerCommand('vscode-editor-group-minimizer.removeFromGroup', group => editorGroupTreeDataProvider.removeFromGroup(group));
+  vscode.commands.registerCommand('vscode-editor-group-minimizer-plus.minimize', () => editorGroupTreeDataProvider.minimize());
+  vscode.commands.registerCommand('vscode-editor-group-minimizer-plus.minimizeAsAdd', () => editorGroupTreeDataProvider.minimizeAsAdd());
+  vscode.commands.registerCommand('vscode-editor-group-minimizer-plus.remove', group => editorGroupTreeDataProvider.remove(group));
+  vscode.commands.registerCommand('vscode-editor-group-minimizer-plus.restore', group => editorGroupTreeDataProvider.restore(group));
+  vscode.commands.registerCommand('vscode-editor-group-minimizer-plus.rename', group => editorGroupTreeDataProvider.rename(group));
+  vscode.commands.registerCommand('vscode-editor-group-minimizer-plus.addToGroup', uri => editorGroupTreeDataProvider.addToGroup(uri));
+  vscode.commands.registerCommand('vscode-editor-group-minimizer-plus.removeFromGroup', group => editorGroupTreeDataProvider.removeFromGroup(group));
 
   context.subscriptions.push(editorGroupTreeDataProvider);
 }
